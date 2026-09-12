407e9ff89244f036e5ac0262ec67bf1269162219
zhihqz <zhihqz@gmail.com>
2026-09-12 10:53:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -158,9 +158,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-      - run: python -c "import os; print('token len:', len(os.environ['PUSHPLUS_TOKEN']))"
 
-      - run: python main.py
-        env:
-          PUSHPLUS_TOKEN: ${{ secrets.PUSHPLUS_TOKEN }}
-          DEEPSEEK_API_KEY: ${{ secrets.DEEPSEEK_API_KEY }}
